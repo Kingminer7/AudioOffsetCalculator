1c62605569141137293f47b3fbe7c50bcc4eade0
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-10-10 14:12:00 +0000

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,68 @@
-#include "Geode/binding/CCTextInputNode.hpp"
-#include <Geode/binding/ButtonSprite.hpp>
-#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
-#include <Geode/ui/Popup.hpp>
+#include <minwindef.h>
+#include <synchapi.h>
+using namespace geode::prelude;
 
 class OffsetCalcPopup : public geode::Popup<CCTextInputNode *> {
 protected:
     bool setup(CCTextInputNode *node) override {
         // convenience function provided by Popup
         // for adding/setting a title to the popup
-        this->setTitle("Hi mom!");
+        setTitle("Audio Sync Calculator");
 
-        auto label = CCLabelBMFont::create(value.c_str(), "bigFont.fnt");
-        label->setPosition(this->m_mainLayer->getContentSize() / 2);
-        this->m_mainLayer->addChild(label);
+        auto startBtn = CCMenuItemSpriteExtra::create(
+			ButtonSprite::create("Start", 0x46, 0, 0.6f, false,"goldFont.fnt","GJ_button_01.png",25.0),
+			this, 
+			menu_selector(OffsetCalcPopup::onStart));
+		startBtn->setID("start-btn");
+		auto syncBtn = CCMenuItemSpriteExtra::create(
+			ButtonSprite::create("Sync", 0x46, 0, 0.6f, false,"goldFont.fnt","GJ_button_02.png",25.0),
+			this, 
+			menu_selector(OffsetCalcPopup::onPress));
+		syncBtn->setID("sync-btn");
+		syncBtn->setVisible(false);
+
+		m_buttonMenu->addChildAtPosition(startBtn, Anchor::Center, {0,-40});
+		m_buttonMenu->addChildAtPosition(syncBtn, Anchor::Center, {0,-40});
+
+		auto lab = TextArea::create("Once you start, press the button whenever the snare sound plays.", "chatFont.fnt", 1.f, 220.f, {0.5, 1}, 15.f, false);
+		m_mainLayer->addChildAtPosition(lab, Anchor::Top, {0, -40});
+
+		FMODAudioEngine::sharedEngine()->m_backgroundMusicChannel->setPaused(true);
 
         return true;
     }
+
+	void onClose(CCObject *sender) override {
+		Popup::onClose(sender);
+		FMODAudioEngine::sharedEngine()->m_backgroundMusicChannel->setPaused(false);
+	}
+
+	void onPress(CCObject *) {
+
+	}
+
+	int press1 = 0;
+	int press2 = 0;
+	int press3 = 0;
+	int press4 = 0;
+
+	void onStart(CCObject *) {
+		FMODAudioEngine::sharedEngine()->playEffect("kick.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("snare.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("kick.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("snare.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("kick.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("snare.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("kick.ogg"_spr);
+		Sleep(500);
+		FMODAudioEngine::sharedEngine()->playEffect("snare.ogg"_spr);
+	}
 
 public:
     static OffsetCalcPopup* create(CCTextInputNode *node) {
@@ -30,8 +77,6 @@ public:
     }
 };
 
-using namespace geode::prelude;
-
 #include <Geode/modify/MoreOptionsLayer.hpp>
 class $modify(CalcOptionsLayer, MoreOptionsLayer) {
 	struct Fields {
@@ -44,7 +89,7 @@ class $modify(CalcOptionsLayer, MoreOptionsLayer) {
 		}
 
 		m_fields->offsetCalc = CCMenuItemSpriteExtra::create(
-			// thanks robtop
+			// thanks robtop (i stole- "borrowed" your code)
 			ButtonSprite::create("Sync",0x46,0,0.6,false,"goldFont.fnt","GJ_button_04.png",25.0),
 			this,
 			menu_selector(CalcOptionsLayer::onCalculateOffset)
@@ -52,7 +97,9 @@ class $modify(CalcOptionsLayer, MoreOptionsLayer) {
 		m_fields->offsetCalc->setID("offset-calc");
 		m_fields->offsetCalc->setVisible(false);
 		m_fields->offsetCalc->setPosition({-40, -105});
+
 		getChildByIDRecursive("fmod-debug-button")->setPositionY(-105);
+
 		m_buttonMenu->addChild(m_fields->offsetCalc);
 
 		return true;
